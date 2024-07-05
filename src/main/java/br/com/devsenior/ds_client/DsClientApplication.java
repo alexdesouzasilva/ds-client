242595c6563a41944784bc7f3ca42772d9c6e9e9
Alex <alexdesouzasilva94@gmail.com>
2024-07-05 05:22:00 -0300

    initial project

---
+++ src/main/java/br/com/devsenior/ds_client/DsClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsenior.ds_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devsenior/ds_client/DsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsenior.ds_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
